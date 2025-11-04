be835726cfca682fe973a07cbcc875cb32c5bc24
Thiago Fernandes <thiagomateus123@hotmail.com>
2025-11-04 20:27:23 -0300

    Configurando para o vercel

--- src/server.js
+++ src/server.js
@@ -15,6 +15,11 @@ server.register(cors, {
 
 server.register(monstersRoutes)
 
-server.listen({port: process.env.PORT}, ()=>{
-    console.log("🚀 Servidor Iniciado!!!")
-})
+export default async function handler(req, res) {
+    await server.ready()
+    server.server.emit("request", req, res)
+}
+
+// server.listen({port: process.env.PORT}, ()=>{
+//     console.log("🚀 Servidor Iniciado!!!")
+// })
